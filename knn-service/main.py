bfd2a791ac4c33b177746d4bd601c80cb326a937
Алдияр Сейлханов <seylkhanov.aldiyar@gmail.com>
2024-05-16 23:02:24 +0500

    feat: improved algorithm to take less then 10 second

--- knn-service/main.py
+++ knn-service/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from Model.KNN import generateRecommendationIds, getPlacesByIds
+from Model.KNN_improved import generateRecommendation
 from typing import List
 from pydantic import BaseModel
 
@@ -25,8 +26,9 @@ async def get_recommendation(user_id:UserId):
     if not user_id:
         raise HTTPException(status_code=400, detail="Ид пользователя не отправлен")
     
-    recids = generateRecommendationIds(user_id.user_id)
-    recommended_places = getPlacesByIds(recids)
+    # recids = generateRecommendationIds(user_id.user_id)
+    # recommended_places = getPlacesByIds(recids)
+    recommended_places = generateRecommendation(user_id.user_id)
     return recommended_places
 
 if __name__ == "__main__":
